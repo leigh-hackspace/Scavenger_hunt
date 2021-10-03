d08d38286c47c72672e3d8a4a126fba2a0d8fecf
John <2320804+ponix4k@users.noreply.github.com>
2021-10-03 01:24:40 +0100

    Add images to ghosts pages and hunter pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,19 @@
 Rails.application.routes.draw do
-	root "hunters#index"
+	root "hunters#welcome"
+
 
 	get "/ghosts", to: "ghosts#index"
+	get "/h4d3s", to: "ghosts#admin_page"
 
 	## ghostbusters show all available ghosts
 	get "/ghosts/egon", to: "ghosts#all"
 	get "/ghosts/peter", to: "ghosts#all"
 	get "/ghosts/raymond", to: "ghosts#all"
 	get "/ghosts/winston", to: "ghosts#all"
+	get "/ghosts/pacman", to: "ghosts#all"
 
 	get "/ghosts/:ghost_uuid", to: "ghosts#show"
 
-	get "/hunters", to: "hunters#index"
+	get "/hunters", to: "hunters#welcome"
+
 end
