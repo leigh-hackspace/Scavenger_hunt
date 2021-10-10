5ddf89a413befe869a9cf03e754920db1d0ea7c3
phyushin <phyushin@gmail.com>
2021-10-10 21:00:57 +0100

    Add upload ghost image functionality, Rework ghost display

--- Gemfile
+++ Gemfile
@@ -55,3 +55,4 @@ end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'simple_form', '~> 5.1'
+gem "image_processing",">=1.2"
--- app/controllers/ghosts_controller.rb
+++ app/controllers/ghosts_controller.rb
@@ -14,15 +14,16 @@ class GhostsController < ApplicationController
   def create
     @ghost = Ghost.new(ghost_params)
     @ghost.ghost_uuid = SecureRandom.uuid
+    #@ghost.image(@ghost.ghost_uuid + ".png")
 
     if @ghost.save
-      redirect_to @ghost
+      redirect_to ghost_path(@ghost.ghost_uuid)
     else
       render :new
     end
   end
 
   def ghost_params
-    params.require(:ghosts).permit(:title, :body)
+    params.require(:ghost).permit(:title, :body, :image)
   end
 end
