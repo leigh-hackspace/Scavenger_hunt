e92e41f88bd8f3aec5b5f6f9fe26a1e3adf81034
phyushin <phyushin@gmail.com>
2021-10-10 17:02:19 +0100

    Fix hunter creation

--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,21 @@
 Rails.application.routes.draw do
 	root "hunters#welcome"
+	get "hunters/all", to: "hunters#all"
 
+	resources :hunters, param: :hunter_uuid
+	resources :ghosts, param: :ghost_uuid
 
 	get "/ghosts", to: "ghosts#index"
 	get "/h4d3s", to: "ghosts#admin_page"
 	post "/h4d3s", to: "ghosts#create"
 
 	## ghostbusters show all available ghosts
-	get "/ghosts/egon", to: "ghosts#all"
-	get "/ghosts/peter", to: "ghosts#all"
-	get "/ghosts/raymond", to: "ghosts#all"
-	get "/ghosts/winston", to: "ghosts#all"
-	get "/ghosts/pacman", to: "ghosts#all"
+	get "/egon", to: "ghosts#all"
+	get "/peter", to: "ghosts#all"
+	get "/raymond", to: "ghosts#all"
+	get "/winston", to: "ghosts#all"
+	get "/pacman", to: "ghosts#all"
 
-	get "/ghosts/:ghost_uuid", to: "ghosts#show"
-
-	get "/hunters/register", to: "hunters#register"
-
-	post "hunters/register", to: "hunters#create"
-	post "/hunters/register/:hunter_name", to: "hunters#create"
-
-
-	get "hunters/all", to: "hunters#index"
-	get "hunters/:hunter_uuid", to: "hunters#show"
+	#get "/ghosts/:ghost_uuid", to: "ghosts#show"
 	get "/hunters", to: "hunters#welcome"
-	
-	
-
 end
