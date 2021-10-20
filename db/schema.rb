188021bf08a0c1e8de26e480f5d04e002ce7d9c0
pixilala <Charlotte.Carter86@gmail.com>
2021-10-20 01:42:11 +0100

    Add captures table for linking hunters to captured ghosts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_19_173117) do
+ActiveRecord::Schema.define(version: 2021_10_20_003924) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,13 @@ ActiveRecord::Schema.define(version: 2021_10_19_173117) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "captures", force: :cascade do |t|
+    t.string "hunter_uuid"
+    t.string "ghost_uuid"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "ghosts", force: :cascade do |t|
