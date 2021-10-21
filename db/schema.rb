4b9bbe47c536f1de7bc4667c498f9a1f57cbef08
pixilala <Charlotte.Carter86@gmail.com>
2021-10-21 01:07:46 +0100

    Add new schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2021_10_20_233306) do
+=======
+ActiveRecord::Schema.define(version: 2021_10_19_173117) do
+>>>>>>> cb39588257408efd2e39e08288b65c35932156b2
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,13 +45,6 @@ ActiveRecord::Schema.define(version: 2021_10_20_233306) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
-  end
-
-  create_table "captures", force: :cascade do |t|
-    t.string "hunter_uuid"
-    t.string "ghost_uuid"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "ghosts", force: :cascade do |t|
