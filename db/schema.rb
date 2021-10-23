c810a07e0bfbf685361058433c48b2de61d53966
phyushin <phyushin@gmail.com>
2021-10-23 19:29:42 +0100

    Add Capture functionality

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_23_134900) do
+ActiveRecord::Schema.define(version: 2021_10_23_180415) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,21 +43,19 @@ ActiveRecord::Schema.define(version: 2021_10_23_134900) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "captures", force: :cascade do |t|
-    t.bigint "ghost_id"
-    t.bigint "patient_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["ghost_id"], name: "index_captures_on_ghost_id"
-    t.index ["patient_id"], name: "index_captures_on_patient_id"
-  end
-
   create_table "ghosts", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.string "ghost_uuid", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "ghosts_hunters", id: false, force: :cascade do |t|
+    t.bigint "hunter_id"
+    t.bigint "ghost_id"
+    t.index ["ghost_id"], name: "index_ghosts_hunters_on_ghost_id"
+    t.index ["hunter_id"], name: "index_ghosts_hunters_on_hunter_id"
   end
 
   create_table "hunters", force: :cascade do |t|
