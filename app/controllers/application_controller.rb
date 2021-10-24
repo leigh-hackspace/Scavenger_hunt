74835a6eb2835b491b922629aeb2ea20e7eafb4a
phyushin <phyushin@gmail.com>
2021-10-25 00:38:34 +0100

    Add ability to tweet captures

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,19 @@ class ApplicationController < ActionController::Base
   def custom_headers
     response.set_header('X-Clacks-Overhead', 'GNU Terry Pratchett') 
   end
+
+  def tweet_capture(hunter_name, ghost_name)
+    client = Twitter::REST::Client.new do |config|
+      config.consumer_key        =  ENV['TWITTER_API_KEY']
+      config.consumer_secret     =  ENV['TWITTER_SECRET_KEY']
+      config.access_token        =  ENV['TWITTER_ACCESS_TOKEN']
+      config.access_token_secret =  ENV['TWITTER_ACCESS_TOKEN_SECRET']
+    end
+
+    if ActiveModel::Type::Boolean.new.cast(ENV['SEND_TWEET'])
+      client.update("Looks like #{hunter_name} has just captured a ghost! 👻#{ghost_name}👻 #spookyhunt2021 ")
+    end
+
+  end
+
 end
