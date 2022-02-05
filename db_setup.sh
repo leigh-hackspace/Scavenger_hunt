f5b0bdb81b816ff9e7ee40a9f4025aa6b577026d
John Williams <john.williams@kane.co.uk>
2022-02-05 16:36:26 +0000

    Create Database Setup Script

---
+++ db_setup.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+read -p "Enter Desired Database Name: " dbName
+read -p "Enter Desired Username: " dbUser
+read -p "Enter Desired Password: " dbPass
+
+echo "Database name: $dbName"
+echo "Username: " $dbUser
+echo "Password: " $dbPass
+
+
+#echo "## Scavenger Hunt Config" >> ~/.bashrc
+#echo "" >> ~/.bashrc
+#echo "export SCAVENGER_DB_NAME='$dbName'" >> ~/.bashrc
+#echo "export SCAVENGER_DB_USER='$dbUser'" >> ~/.bashrc
+#echo "export SCAVENGER_DB_PASS='$dbPass'" >> ~/.bashrc
+
+#source ~/.bashrc
+
+#echo $SCAVENGER_DB_NAME
+#echo $SCAVENGER_DB_USER
+#echo $SCAVENGER_DB_PASS
+
+set -e
+
+sudo su postgres <<EOF
+createdb  $dbName;
+psql -c "CREATE USER $dbUser WITH PASSWORD '$dbPass';"
+psql -c "grant all privileges on database $dbName to $dbUser;"
+echo "Postgres User '$dbUser' and database '$dbName' created."
+EOF
+
