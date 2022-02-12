4413237e87ed98c5354491953ae08ced157d944f
phyushin <phyushin@gmail.com>
2022-02-12 16:57:57 +0000

    Add basic rspec tests

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it("should generate unique uuid") do
+    expect(
+      Item.create()
+    ).not_to match(Item.create())
+  end
+  
 end
