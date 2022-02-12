9eb86db5c65575141620edf89ea9a914f63d1666
John Williams <john.williams@kane.co.uk>
2022-02-12 18:41:02 +0000

    changes to the Easter Css

--- db_setup.sh
+++ db_setup.sh
@@ -1,21 +1,21 @@
 #! /bin/bash
 
-read -p "Enter Desired Database Name: " dbName
-read -p "Enter Desired Username: " dbUser
-read -p "Enter Desired Password: " dbPass
+#read -p "Enter Desired Database Name: " dbName
+#read -p "Enter Desired Username: " dbUser
+#read -p "Enter Desired Password: " dbPass
 
-echo "Database name: $dbName"
-echo "Username: " $dbUser
-echo "Password: " $dbPass
+#echo "Database name: $dbName"
+#echo "Username: " $dbUser
+#echo "Password: " $dbPass
 
 
-echo "## Scavenger Hunt Config" >> ~/.bashrc
-echo "" >> ~/.bashrc
-echo "export SCAVENGER_DB_NAME='$dbName'" >> ~/.bashrc
-echo "export SCAVENGER_DB_USER='$dbUser'" >> ~/.bashrc
-echo "export SCAVENGER_DB_PASS='$dbPass'" >> ~/.bashrc
+#echo "## Scavenger Hunt Config" >> ~/.bashrc
+#echo "" >> ~/.bashrc
+#echo "export SCAVENGER_DB_NAME='$dbName'" >> ~/.bashrc
+#echo "export SCAVENGER_DB_USER='$dbUser'" >> ~/.bashrc
+#echo "export SCAVENGER_DB_PASS='$dbPass'" >> ~/.bashrc
 
-source ~/.bashrc
+#source ~/.bashrc
 
 echo $SCAVENGER_DB_NAME
 echo $SCAVENGER_DB_USER
@@ -24,9 +24,8 @@ echo $SCAVENGER_DB_PASS
 set -e
 
 sudo su postgres <<EOF
-createdb  $dbName owner $dbUser;
-psql -c "CREATE USER $dbUser WITH ENCRYPTED PASSWORD '$dbPass';"
-psql -c "grant all privileges on database $dbName to $dbUser;"
-echo "Postgres User '$dbUser' and database '$dbName' created."
+psql -c "CREATE USER 'hunt_admin; WITH ENCRYPTED PASSWORD 'Y3llowhat';"
+CREATE DATABASE $dbName owner 'hunt_admin';
+psql -c "grant all privileges on database 'egg_hunt' to 'hunt_admin';"
+echo "Postgres User 'hunt_admin' and database 'egg_hunt' created."
 EOF
-
