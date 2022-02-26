13088067713c2a72e9b938d152eb9b9de3ff1ccf
John <2320804+ponix4k@users.noreply.github.com>
2022-02-26 13:50:03 +0000

    Update db_setup.sh
    separate changes to this branch

--- db_setup.sh
+++ db_setup.sh
@@ -4,29 +4,23 @@ read -p "Enter Desired Database Name: " dbName
 read -p "Enter Desired Username: " dbUser
 read -p "Enter Desired Password: " dbPass
 
-echo "Database name: $dbName"
-echo "Username: " $dbUser
-echo "Password: " $dbPass
+echo "## PROD" >> .env
+echo "DEV_SCAVENGER_DB_NAME={CHANGE-ME}" >> .env
+echo "DEV_SCAVENGER_DB_USER={CHANGE-ME}" >> .env
+echo "DEV_SCAVENGER_DB_PASS={CHANGE-ME}" >> .env
+echo "" >> .env
 
 
-echo "## Scavenger Hunt Config" >> ~/.bashrc
-echo "" >> ~/.bashrc
-echo "export SCAVENGER_DB_NAME='$dbName'" >> ~/.bashrc
-echo "export SCAVENGER_DB_USER='$dbUser'" >> ~/.bashrc
-echo "export SCAVENGER_DB_PASS='$dbPass'" >> ~/.bashrc
-
-source ~/.bashrc
-
-echo $SCAVENGER_DB_NAME
-echo $SCAVENGER_DB_USER
-echo $SCAVENGER_DB_PASS
+echo "## DEV" >> .env
+echo "DEV_SCAVENGER_DB_NAME="$dbName >> .env
+echo "DEV_SCAVENGER_DB_USER="$dbUser >> .env
+echo "DEV_SCAVENGER_DB_PASS="$dbPass >> .env
+echo "" >> .env
 
 set -e
 
 sudo su postgres <<EOF
-createdb  $dbName owner $dbUser;
 psql -c "CREATE USER $dbUser WITH ENCRYPTED PASSWORD '$dbPass';"
+psql -c "CREATE DATABASE $dbName owner $dbUser;"
 psql -c "grant all privileges on database $dbName to $dbUser;"
-echo "Postgres User '$dbUser' and database '$dbName' created."
-EOF
-
+echo "Postgres User $dbUser and database $dbName created."
