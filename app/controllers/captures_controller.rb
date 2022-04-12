3ba1feafcdb55fcecb04e27c0e6d4893705029d6
Alex Hartley <alex@hartley.cc>
2022-04-13 00:13:46 +0100

    Refactor how items are collected (#101)
    Don't abuse StandardError.
    Add very basic bonus/item tests.

--- app/controllers/captures_controller.rb
+++ app/controllers/captures_controller.rb
@@ -5,12 +5,23 @@ class CapturesController < ApplicationController
       check_valid_session(session[:hunter_session_id], @hunter)
       @item = Item.find_by_item_uuid(params[:item_item_uuid])
 
-      @item.capture(@hunter)
-      tweet_capture(@hunter, @item)
+      status = @item.capture(@hunter)
 
-      redirect_to item_captures_path(@item, message: true)
-    rescue => e
-      redirect_to item_captures_path(@item, message: e)
+      case status
+      when :this_bonus_item_has_already_been_claimed
+        status = 1
+      when :you_already_have_a_bonus_item
+        status = 2
+      when :you_already_have_this_item
+        status = 3
+      when :success
+        status = 4
+        tweet_capture(@hunter, @item)
+      else
+        status = 5
+      end
+
+      redirect_to item_captures_path(@item, message: status)
     end
   end
 end
--- spec/models/hunter_spec.rb
+++ spec/models/hunter_spec.rb
@@ -3,6 +3,10 @@
 require 'rails_helper'
 
 RSpec.describe Hunter, type: :model do
+  it "has a valid factory" do
+    expect(create(:hunter)).to be_valid
+  end
+
   it('should generate unique uuid') do
     expect(
       Hunter.create(hunter_name: 'test', password: 'password1')
