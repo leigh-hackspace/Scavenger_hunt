6c40c3cc643c258d2ddd7eac4adbc044f904e826
alexhartley <alex@hartley.cc>
2022-04-13 16:58:46 +0100

    Move status decider to be a first class citizen

--- app/controllers/captures_controller.rb
+++ app/controllers/captures_controller.rb
@@ -5,26 +5,8 @@ class CapturesController < ApplicationController
     @item = Item.find_by_item_uuid(params[:item_item_uuid])
 
     status = @item.capture(@hunter)
-    status = decide_status(status)
+    result = Status.decide(status)
 
-    redirect_to item_captures_path(@item, message: status)
-  end
-
-  private
-
-  def decide_status(status)
-    case status
-    when :this_bonus_item_has_already_been_claimed
-      1
-    when :you_already_have_a_bonus_item
-      2
-    when :you_already_have_this_item
-      3
-    when :success
-      4
-      tweet_capture(@hunter, @item)
-    else
-      5
-    end
+    redirect_to item_captures_path(@item, message: result)
   end
 end
---
+++ app/models/status.rb
@@ -0,0 +1,29 @@
+class Status
+  def self.decide(status)
+    self.new(status).decide
+  end
+
+  def initialize(status)
+    @status = status
+  end
+
+  def decide
+    case status
+    when :this_bonus_item_has_already_been_claimed
+      1
+    when :you_already_have_a_bonus_item
+      2
+    when :you_already_have_this_item
+      3
+    when :success
+      4
+      tweet_capture(@hunter, @item)
+    else
+      5
+    end
+  end
+
+  private
+
+  attr_reader :status
+end
