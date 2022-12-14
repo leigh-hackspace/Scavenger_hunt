de45c355eec0ddcf082d139b8775b8734de1697d
John W <john@leighhack.org>
2022-12-14 01:29:38 +0000

    merge deploy back into main

--- app/helpers/captures_helper.rb
+++ app/helpers/captures_helper.rb
@@ -9,14 +9,4 @@ module CapturesHelper
     gifs[rand(gifs.length)]
   end
 
-  def show_random_easter_gif
-    gifs = %w{
-    https://media.tenor.co/images/292397abd35cb49365f6a57e8bb8e47d/raw
-    https://media3.giphy.com/media/l2R00wfa8fttlpRPG/giphy.gif?cid=790b7611daba2e4c365af6f26a7d9b76e84f174f0b8987e6&rid=giphy.gif
-    https://media1.giphy.com/media/26gsiGuPlP95pumwE/giphy.gif?cid=790b7611373c059b8461eb195c190a6ba5603ba601491fd3&rid=giphy.gif
-    https://media3.giphy.com/media/PDPkDE1UrKclW/giphy.gif?cid=790b7611c1a3446dc39f725c2721513d051aedb1ff245e5c&rid=giphy.gif
-    }
-
-    gifs[rand(gifs.length)]
-  end
 end
